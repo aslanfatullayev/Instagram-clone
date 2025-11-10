a6f571c2bc701b416135ae02c0be3f46dcbf3d82
aslanfatullayev <aslanfatullev2006@gmail.com>
2025-11-10 17:11:11 +0500

    Update

---
+++ users/serializers.py
@@ -0,0 +1,34 @@
+from .models import User, UserConfirmation, VIA_EMAIL, VIA_PHONE, NEW, CODE_VERIFIED, DONE, PHOTO_STEP
+from rest_framework import exceptions
+from django.db.models import Q
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+class SignUpSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    
+    def __init__(self, *args, **kwargs):
+        super(SignUpSerializer, self). __init__(*args, **kwargs)
+        self.fields['email_phone_number'] = serializers.CharField(required = False)
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'auth_type',
+            'auth_status'
+        )
+        extra_kwargs = {
+            'auth-type': {'ready_only': True, 'required': False},
+            'auth_status': {'read_only': True, 'required':False}
+        }
+
+def validate(self, data):
+    super(SignUpSerializer, self).validate(data)
+    data = self.auth_validate(data)
+    return data
+
+@staticmethod
+def auth_validate(attrs):
+    print(attrs)
+    user_input = str(attrs.get('email_phone_number')).lower
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from .serializers import SignUpSerializer
+from .models import User
+from rest_framework import permissions
+from rest_framework.decorators import permission_classes
 
-# Create your views here.
+class CreateUserView(CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes =(permissions.AllowAny)
+    serializer_class = SignUpSerializer
