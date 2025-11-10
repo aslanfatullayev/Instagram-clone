27c0ff6abdb2b35a3b8409a5a94953cd534b1ef9
aslanfatullayev <aslanfatullev2006@gmail.com>
2025-11-10 14:40:27 +0500

    User save method override

--- users/models.py
+++ users/models.py
@@ -1,13 +1,15 @@
 import random
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from shared.models import BaseModel
 from datetime import datetime, timedelta
 from django.core.validators import FileExtensionValidator
+from rest_framework_simplejwt.tokens import RefreshToken
 
 ORDINARY_USER = "ordinary_user"
 MANAGER = "manager"
-ADMIN = 'admin'
+ADMIN = 'admin' 
 VIA_EMAIL = 'via_email'
 VIA_PHONE = 'via_phone'
 NEW = 'new'
@@ -55,6 +57,47 @@ class User(AbstractUser, BaseModel):
         )
         return code
     
+    def check_username(self):
+        if not self.username:
+            temp_username = f'instagram-{uuid.uuid4().__str__().split("-")[-1]}'
+            while User.objects.filter(username = temp_username):
+                temp_username = f"{temp_username}{random.randint(0,9)}"
+            self.username = temp_username
+
+    def check_email(self):
+        if self.email:
+            normalize_email = self.email.lower() #FatuLLaev@gmail.com => fatullaev@gmail.com
+            self.email =  normalize_email
+
+    def check_pass(self):
+        if not self.password:
+            temp_password = f"password-{uuid.uuid4().__str__().split("-")[-1]}"
+            self.password = temp_password
+
+    def hashing_password(self):
+        if not self.password.startswith('pbkdf2_sha256'):
+            self.set_password(self.password)
+
+    def token(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            "access": str(refresh.access_token),
+            "refresh_token": str(refresh)
+        }
+
+def clean(self):
+    self.check_email()
+    self.check_username()
+    self.check_pass()
+    self.hashing_password()
+
+def save(self, *args, **kwargs):
+    if not self.pk:
+        self.clean()
+    super(User,self).save(*args, **kwargs)
+
+
+
 PHONE_EXPIRE = 2
 EMAIL_EXPIRE = 5
 
@@ -80,4 +123,4 @@ class UserConfirmation(BaseModel):
                 self.expiration_time = datetime.now() + timedelta(minutes = EMAIL_EXPIRE)
             else:
                 self.expiration_time = datetime.now() + timedelta(minutes=PHONE_EXPIRE)
-        super(UserConfirmation, self).save(*args, **kwargs)
+        super(UserConfirmation, self).save(*args, **kwargs) 
